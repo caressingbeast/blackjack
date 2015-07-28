aad847ce542f9fb81e65d1ea84f35164675ca342
Brandon Burning <bnburning3@gmail.com>
2015-07-28 13:17:04 -0700

    Initialize repository

---
+++ js/hello.js
@@ -0,0 +1,16 @@
+(function () {
+  'use strict';
+
+  var _this;
+
+  var Blackjack = {
+
+    init: function () {
+
+      // cache object reference
+      _this = this;
+    }
+  };
+
+  Blackjack.init();
+})();
