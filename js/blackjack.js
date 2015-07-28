a0e5f0aacf43c2b30943be1db10a075f44bee641
Brandon Burning <bnburning3@gmail.com>
2015-07-28 13:35:55 -0700

    Random stuff

--- js/blackjack.js
+++ js/blackjack.js
@@ -9,6 +9,22 @@
 
       // cache object reference
       _this = this;
+
+      // cache DOM elements
+      _this.$dealer = $('.dealer-cards');
+      _this.$player = $('.player-cards');
+
+      // set up cards
+      _this.ranks = ['a', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'j', 'q', 'k'];
+      _this.suits = ['c', 'd', 'h', 's'];
+
+      _this.makeDecks(6);
+      _this.shuffleDecks();
+    },
+
+    makeDecks: function (count) {
+      var m = _this.ranks.length * _this.suits.length;
+      console.log(count, m);
     }
   };
 
